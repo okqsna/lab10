e4207bf6338afea915e192dfdb3c6435e2bffe10
Oksana Moskviak <moskviak.pn@ucu.edu.ua>
2025-03-26 10:48:12 +0200

    first task commit full

---
+++ linked list to str/linked_list_to_string.py
@@ -0,0 +1,17 @@
+class Node():
+    def __init__(self, data, next = None):
+        self.data = data
+        self.next = next
+
+def stringify(node):
+    if not isinstance(node, Node):
+        return 'None'
+    nodes = ''
+    while node:
+        nodes += str(node.data) + ' -> '
+        if node.next == None:
+            nodes += 'None'
+        node = node.next
+    return nodes
+
+print(stringify(Node(0, Node(1, Node(2, Node(3))))))
