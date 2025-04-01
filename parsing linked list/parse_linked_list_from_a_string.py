9790c83c45be094f1fd3d8a4ba66ae325b52c625
Oksana Moskviak <moskviak.pn@ucu.edu.ua>
2025-04-02 02:15:45 +0300

    updates

--- parsing linked list/parse_linked_list_from_a_string.py
+++ parsing linked list/parse_linked_list_from_a_string.py
@@ -3,11 +3,21 @@ class Node:
         self.data = data
         self.next = next
 
+    def __repr__(self):
+        return f'{self.data} -> {self.next}'
+
 def linked_list_from_string(s):
     new_list = s.split(' -> ')
-    for i, el in enumerate(new_list):
-        res = Node(el, new_list[i])
-    return res
+    if len(new_list) == 0 or new_list[0] == 'None':
+        return None
+    head = Node(int(new_list[0]))
+    curr = head
+
+    for el in new_list[1:]:
+        if el != 'None':
+            curr.next = Node(int(el))
+            curr = curr.next
+    return head
 
 print(linked_list_from_string("1 -> 2 -> 3 -> None"))
 # Node(1, Node(2, Node(3)))
