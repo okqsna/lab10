51817083ad26ab621cc61f79adf7ddf90feee2d5
Oksana Moskviak <moskviak.pn@ucu.edu.ua>
2025-04-02 21:50:52 +0300

    get nth task uploaded (not all tests passed)

--- get nth/get_nth.py
+++ get nth/get_nth.py
@@ -1,10 +1,20 @@
+"""
+Getting nth from LinkedList
+"""
+
 def get_nth(node, index):
+    if index < 0 or node is None:
+        raise IndexError
+
     curr_el = node
     counter = 0
+
     while curr_el:
         if counter == index:
             return curr_el
+        if curr_el is not None:
+            counter += 1
         curr_el = curr_el.next
-        counter += 1
-    if counter < index or index < 0 or node is None:
+
+    if counter < index:
         raise IndexError
