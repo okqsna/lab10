48d89591648d7cb45943a07567d3d52feab4942d
Oksana Moskviak <moskviak.pn@ucu.edu.ua>
2025-04-02 21:52:07 +0300

    all tests passed get_nth

--- get nth/get_nth.py
+++ get nth/get_nth.py
@@ -3,18 +3,17 @@ Getting nth from LinkedList
 """
 
 def get_nth(node, index):
+    # Your code goes here.
     if index < 0 or node is None:
-        raise IndexError
-
+        raise IndexError  
     curr_el = node
     counter = 0
 
     while curr_el:
         if counter == index:
             return curr_el
-        if curr_el is not None:
+        if curr_el.next is not None:
             counter += 1
         curr_el = curr_el.next
-
     if counter < index:
         raise IndexError
