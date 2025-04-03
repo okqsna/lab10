8d76f1683a83a4a7860ac543aa1d729e934e51b1
Oksana Moskviak <moskviak.pn@ucu.edu.ua>
2025-04-04 00:03:21 +0300

    start commit

---
+++ recursive_reverse/recursive_reverse.py
@@ -0,0 +1,16 @@
+class Node(object):
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+def reverse(head):
+    if head is None:
+        return None
+    if head.next is None:
+        return head
+
+    curr = head
+    curr.next = curr
+    
+    
+    return curr
