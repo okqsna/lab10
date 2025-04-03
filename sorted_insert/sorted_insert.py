b677b837d45280a8acb623b9e6a310363ceeb41e
Oksana Moskviak <moskviak.pn@ucu.edu.ua>
2025-04-03 13:07:59 +0300

    Add files via upload

--- sorted_insert/sorted_insert.py
+++ sorted_insert/sorted_insert.py
@@ -18,7 +18,7 @@ def sorted_insert(head, data):
     while curr_node.next and curr_node.next.data < data:
         curr_node = curr_node.next
 
-    curr_node.next = node_to_be_inserted
     node_to_be_inserted.next = curr_node.next
+    curr_node.next = node_to_be_inserted
 
     return head
